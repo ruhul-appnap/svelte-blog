32400915109e4b9a68337001c82b46a6828f9054
Ruhul <ruhul@appnap.io>
2024-12-05 17:36:45 +0600

    extract prams

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
-  
-  const {data} = $props();
 
+  const {data} = $props();
+  import { page } from '$app/stores';
+
+  const blogId = $page.params;
   $inspect(data.blogData)
 </script>
 
 <div>
   blog page
   {JSON.stringify(data.blogData)}
+
+  {JSON.stringify(blogId)}
 </div>
